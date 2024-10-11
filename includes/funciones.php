c5d17e66fd7f4db43e463cd03a1b0a1f150f0e93
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-10-11 11:38:29 -0500

    se creo los archivos para el CRUD

--- includes/funciones.php
+++ includes/funciones.php
@@ -5,3 +5,28 @@ require 'app.php';
 function incluirTemplate( string $nombre, bool $inicio = false ) {
     include TEMPLATES_URL . "/${nombre}.php";
 }
+
+
+
+
+
+
+// Funcion si se usa en el subdirectorio APACHE 🪶
+function obtenerNombreDirectorioTrabajo(string $ruta){
+    // Obtenemos la ruta completa del directorio principal y reemplazamos las barras invertidas por diagonales en caso de usar Windows
+    $ruta_completa = str_replace("\\", "/", dirname(__DIR__));
+
+    // Dividimos la ruta completa en partes usando la barra diagonal como delimitador
+    $partes_ruta = explode("/", $ruta_completa);
+
+    // Obtenemos el nombre del directorio principal (el último elemento del array de partes de la ruta)
+    $ultimo_directorio = end($partes_ruta);
+
+    // Concatenamos el nombre del directorio principal con la parte específica de la ruta y la retornamos
+    return "/" . $ultimo_directorio . $ruta;
+}
+/* Se usa de esta manera 👇
+
+<link rel="stylesheet" href="<?php echo obtenerNombreDirectorioTrabajo("/build/css/app.css") ?>">
+
+*/
