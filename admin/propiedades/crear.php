25845fb7cd04ebb42757ed6b99aeccacf8642d9e
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-10-11 11:55:08 -0500

    se creo el html para el formulario de crear propiedades

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -7,6 +7,57 @@
         <h1>Crear</h1>
         
         <a href="/admin" class="boton boton-verde">Volver</a>
+    
+        <form class="formulario" method="POST" action="/admin/propiedades/crear.php" enctype="multipart/form-data">
+            <fieldset>
+                <legend>Información General</legend>
+
+                <label for="titulo">Titulo:</label>
+                <input type="text" id="titulo" name="titulo" placeholder="Titulo Propiedad" value="<?php echo $titulo; ?>">
+
+                <label for="precio">Precio:</label>
+                <input type="number" id="precio" name="precio" placeholder="Precio Propiedad" value="<?php echo $precio; ?>">
+
+                <label for="imagen">Imagen:</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png" name="imagen">
+
+                <label for="descripcion">Descripción:</label>
+                <textarea id="descripcion" name="descripcion"><?php echo $descripcion; ?></textarea>
+
+            </fieldset>
+
+            <fieldset>
+                <legend>Información Propiedad</legend>
+
+                <label for="habitaciones">Habitaciones:</label>
+                <input 
+                    type="number" 
+                    id="habitaciones" 
+                    name="habitaciones" 
+                    placeholder="Ej: 3" 
+                    min="1" 
+                    max="9" 
+                    value="<?php echo $habitaciones; ?>">
+
+                <label for="wc">Baños:</label>
+                <input type="number" id="wc" name="wc" placeholder="Ej: 3" min="1" max="9" value="<?php echo $wc; ?>">
+
+                <label for="estacionamiento">Estacionamiento:</label>
+                <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej: 3" min="1" max="9" value="<?php echo $estacionamiento; ?>">
+
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+
+                <select name="vendedor">
+                    <option value="1">Anderson</option>
+                    <option value="2">Yunni</option>
+                </select>
+            </fieldset>
+
+            <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+        </form>
     </main>
 
 
