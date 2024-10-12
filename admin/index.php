8095a0a3d7f1a123018b93d4167c73b802865f30
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-10-12 18:35:57 -0500

    se agrego una tabla para listar las propiedades en el index

--- admin/index.php
+++ admin/index.php
@@ -14,6 +14,31 @@
             
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
 
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la playa</td>
+                    <td> <img src="/imagenes/27f78a775526af6b4cb6fad9768e950f.jpg" class="imagen-tabla"> </td>
+                    <td>$12000</td>
+                    <td>                        
+                        <a href="" class="boton-rojo-block">Eliminar</a>
+                        <a href="" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
 <?php 
