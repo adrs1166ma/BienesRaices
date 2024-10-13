d7c7a2bce93c8ac41572610f1913a554f9031381
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-10-13 12:21:33 -0500

    se agrego validaciones en el formulario

--- login.php
+++ login.php
@@ -1,4 +1,34 @@
-<?php 
+<?php
+
+require 'includes/config/database.php';
+$db = conectarDB();
+// Autenticar el usuario
+
+$errores = [];
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    echo "<pre>";
+    var_dump($_POST);
+    echo "</pre>";
+
+    $email = mysqli_real_escape_string($db,  filter_var( $_POST['email'], FILTER_VALIDATE_EMAIL) );
+    $password = mysqli_real_escape_string($db,  $_POST['password']);
+
+
+    if(!$email) {
+        $errores[] = "El email es obligatorio o no es válido";
+    }
+
+    if(!$password) {
+        $errores[] = "El Password es obligatorio";
+    }
+    echo "<pre>";
+    var_dump($errores);
+    echo "</pre>";
+
+}
+
+    // Incluye el header
     require 'includes/funciones.php';    
     incluirTemplate('header');
 ?>
@@ -6,7 +36,13 @@
     <main class="contenedor seccion contenido-centrado">
         <h1>Iniciar Sesión</h1>
 
-        <form class="formulario" novalidate>
+        <?php foreach($errores as $error): ?>
+            <div class="alerta error">
+                <?php echo $error; ?>
+            </div>
+        <?php endforeach; ?>
+
+        <form method="POST" class="formulario" novalidate>
             <fieldset>
                 <legend>Email y Password</legend>
 
