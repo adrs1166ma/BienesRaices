4ace008000095d0d6e4320c65cf3f7cda05736a1
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-10-28 10:45:35 -0500

    se creo la funcion debuguear  se minifico la funcion de estaAutenticado

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -3,17 +3,9 @@
     require '../../includes/app.php';    
 
     use App\Propiedad;
-    $propiedad = new Propiedad;
 
-    echo "<pre>";
-    var_dump($propiedad);
-    echo "</pre>";
 
-    exit;
-    $auth = estaAutenticado();
-    if(!$auth) {
-        header('Location: /');
-    }
+    estaAutenticado();
     
     $db = conectarDB();
 
