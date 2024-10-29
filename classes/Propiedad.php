ef5da91c4f576c6424751657b01e0cd43e2e7e32
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-10-29 10:45:57 -0500

    se sanitizo la entrada de datos Propiedades

--- classes/Propiedad.php
+++ classes/Propiedad.php
@@ -6,6 +6,7 @@ class Propiedad {
 
     // Base de DATOS
     protected static $db;
+    protected static $columnasDB = ['id', 'titulo', 'precio', 'imagen', 'descripcion', 'habitaciones', 'wc', 'estacionamiento', 'creado', 'vendedorId'];
 
     public $id;
     public $titulo;
@@ -17,6 +18,11 @@ class Propiedad {
     public $estacionamiento;
     public $creado;
     public $vendedorId;
+    
+    // Definir la conexión a la BD
+    public static function setDB($database) {
+        self::$db = $database;
+    }
 
     public function __construct($args = [])
     {
@@ -33,6 +39,9 @@ class Propiedad {
     }
 
     public function guardar() {
+        // Sanitizar los datos
+        $atributos = $this->sanitizarAtributos();
+        debuguear($atributos);
 
         // Insertar en la base de datos
         $query = " INSERT INTO propiedades (titulo, precio, imagen, descripcion, habitaciones, wc, estacionamiento, creado, vendedorId ) VALUES ( '$this->titulo', '$this->precio', '$this->imagen', '$this->descripcion', '$this->habitaciones', '$this->wc', '$this->estacionamiento', '$this->creado', '$this->vendedorId' ) ";
@@ -42,8 +51,24 @@ class Propiedad {
         debuguear($resultado);
     }
 
-    // Definir la conexión a la BD
-    public static function setDB($database) {
-        self::$db = $database;
+    // Identificar y unir los atributos de la BD
+    public function atributos(){
+        $atributos = [];
+        foreach(self::$columnasDB as $columna) {
+            if($columna == 'id') continue;
+            $atributos[$columna] = $this->$columna;
+        }
+        return $atributos;
     }
+
+    public function sanitizarAtributos() {
+        $atributos = $this->atributos();
+        $sanitizado = [];
+        
+        foreach($atributos as $key => $value) {
+            $sanitizado[$key] = self::$db->escape_string($value);
+        }
+        return $sanitizado;
+    }
+
 }
