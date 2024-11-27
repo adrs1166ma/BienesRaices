a76bf000dedf83bccb8e575935598ca3b0dd3a76
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-11-27 06:37:03 -0500

    Se implemento funcion de eliminar propiedades

--- admin/index.php
+++ admin/index.php
@@ -15,21 +15,10 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id = $_POST['id'];
     $id = filter_var($id, FILTER_VALIDATE_INT);
     if($id) {
-        // Eliminar el archivo
-        $query = "SELECT imagen FROM propiedades WHERE id = ${id}";
 
-        $resultado = mysqli_query($db, $query);
-        $propiedad = mysqli_fetch_assoc($resultado);
-        
-        unlink('../imagenes/' . $propiedad['imagen']);
+        $propiedad = Propiedad::find($id);
+        $propiedad -> eliminar();
 
-        // Eliminar la propiedad
-        $query = "DELETE FROM propiedades WHERE id = ${id}";
-        $resultado = mysqli_query($db, $query);
-
-        if($resultado) {
-            header('location: /admin?resultado=3');
-        }
     }
 }
 
